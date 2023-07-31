3fbcdbde102f7b3cf4666464702fb4d87ad4bcd2
kens1ne <tanndph25572@fpt.edu.vn>
2023-07-31 11:00:34 +0700

    fix chapters render

--- src/pages/comic/[ComicId]/[ChapterId].jsx
+++ src/pages/comic/[ComicId]/[ChapterId].jsx
@@ -135,82 +135,80 @@ const Chapter = () => {
               </Link>
             </div>
           </div>
-          {showToolbars && (
-            <div
-              className={`select-none absolute py-2 px-2 bottom-0 inset-x-0 bg-[rgba(0,0,0,0.75)] text-gray-400 text-sm font-semibold duration-300 ${
-                showToolbars
-                  ? "translate-y-0 opacity-1"
-                  : "translate-y-full opacity-0"
-              }`}
-              onClick={handleElementClick}
-            >
-              <div className="max-w-2xl mx-auto">
-                <div className="flex items-center justify-between gap-3">
+          <div
+            className={`select-none absolute py-2 px-2 bottom-0 inset-x-0 bg-[rgba(0,0,0,0.75)] text-gray-400 text-sm font-semibold duration-300 ${
+              showToolbars
+                ? "translate-y-0 opacity-1"
+                : "translate-y-full opacity-0"
+            }`}
+            onClick={handleElementClick}
+          >
+            <div className="max-w-2xl mx-auto">
+              <div className="flex items-center justify-between gap-3">
+                <Link href={`/comic/${ComicId}/`}>
+                  <button
+                    className="p-2 bg-red-500 text-white rounded"
+                    title="Previous"
+                  >
+                    <AiOutlineLeft />
+                  </button>
+                </Link>
+                <div className="flex items-center">
                   <Link href={`/comic/${ComicId}/`}>
-                    <button
-                      className="p-2 bg-red-500 text-white rounded"
-                      title="Previous"
-                    >
-                      <AiOutlineLeft />
+                    <button className="p-2 bg-cyan-500 text-white rounded mr-1">
+                      <AiFillInfoCircle size={18} />
                     </button>
                   </Link>
-                  <div className="flex items-center">
-                    <Link href={`/comic/${ComicId}/`}>
-                      <button className="p-2 bg-cyan-500 text-white rounded mr-1">
-                        <AiFillInfoCircle size={18} />
-                      </button>
-                    </Link>
-                    <button
-                      className="p-2 bg-red-500 text-white rounded"
-                      onClick={handleShowChapters}
+                  <button
+                    className="p-2 bg-red-500 text-white rounded"
+                    onClick={handleShowChapters}
+                  >
+                    <div
+                      className={`z-10 absolute bg-zinc-900 w-60 py-3 rounded bottom-[53px] text-white text-left duration-200 origin-bottom scale-100 left-1/2 transform -translate-x-1/2 ${
+                        showChapters ? "scale-100" : "scale-[0.001]"
+                      }`}
                     >
-                      <div
-                        className={`z-10 absolute bg-zinc-900 w-60 py-3 rounded bottom-[53px] text-white text-left duration-200 origin-bottom scale-100 left-1/2 transform -translate-x-1/2 ${
-                          showChapters ? "scale-100" : "scale-[0.001]"
-                        }`}
+                      <h5 className="text-lg px-4 pb-1">
+                        All: {data?.chapters?.length} Chapters
+                      </h5>
+                      <ul
+                        className="overflow-auto text-sm h-max max-h-72 font-normal"
+                        ref={chaptersListRef}
                       >
-                        <h5 className="text-lg px-4 pb-1">
-                          All: {data?.chapters?.length} Chapters
-                        </h5>
-                        <ul
-                          className="overflow-auto text-sm h-max max-h-72 font-normal"
-                          ref={chaptersListRef}
-                        >
-                          {data?.chapters?.map((chapter, index) => {
-                            return (
-                              <li key={chapter.id}>
-                                <Link
-                                  href={`/comic/${ComicId}/${chapter.id}`}
-                                  className={`py-2 block truncate px-5 duration-100 hover:text-red-500 ${
-                                    chapter.id == ChapterId
-                                      ? "text-red-500 font-bold"
-                                      : ""
-                                  }`}
-                                  onClick={() => setShowToolbars(false)}
-                                >
-                                  {chapter.name}
-                                </Link>
-                              </li>
-                            );
-                          })}
-                        </ul>
-                      </div>
+                        {data?.chapters?.map((chapter, index) => {
+                          return (
+                            <li key={chapter.id}>
+                              <Link
+                                href={`/comic/${ComicId}/${chapter.id}`}
+                                className={`py-2 block truncate px-5 duration-100 hover:text-red-500 ${
+                                  chapter.id == ChapterId
+                                    ? "text-red-500 font-bold"
+                                    : ""
+                                }`}
+                                onClick={() => setShowToolbars(false)}
+                              >
+                                {chapter.name}
+                              </Link>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
 
-                      {data?.chapter_name}
-                    </button>
-                  </div>
-                  <Link href={`/comic/${ComicId}/`}>
-                    <button
-                      className="p-2 bg-red-500 text-white rounded"
-                      title="Next"
-                    >
-                      <AiOutlineRight />
-                    </button>
-                  </Link>
+                    {data?.chapter_name}
+                  </button>
                 </div>
+                <Link href={`/comic/${ComicId}/`}>
+                  <button
+                    className="p-2 bg-red-500 text-white rounded"
+                    title="Next"
+                  >
+                    <AiOutlineRight />
+                  </button>
+                </Link>
               </div>
             </div>
-          )}
+          </div>
         </div>
       </main>
     </>
